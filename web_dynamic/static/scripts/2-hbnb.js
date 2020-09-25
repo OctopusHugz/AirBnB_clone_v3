3147b50208eb3b608c03cb2b9dca80d461930b3c
Kelsie Merchant <eislek02@gmail.com>
2020-09-25 21:27:47 +0000

    Add files for task 3; Add new div element to header tag for api_status with CSS styling and add class available to switch between depending on jQuery AJAX API request status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+const $ = window.$;
+const amenityDict = {};
+$(document).ready(function () {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (status) {
+     if (status === 'OK') {
+	 $('DIV#api_status').addClass('available');
+     } else {
+	 $('DIV#api_status').removeClass('available');	 
+     }
+  });
+
+    $('li input').click(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').empty();
+    const amenityDictKeys = Object.keys(amenityDict);
+    const amenityDictLength = amenityDictKeys.length;
+    amenityDictKeys.forEach(function (key, index) {
+      $('.amenities h4').append(amenityDict[key]);
+      if (index !== amenityDictLength - 1) {
+        $('.amenities h4').append(', ');
+      }
+    });
+    if (amenityDictLength === 0) {
+      $('.amenities h4').append('&nbsp;');
+    }
+  });
+});
