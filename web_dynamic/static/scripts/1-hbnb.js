2d4deb8ec99f3731ce2a11e5ae96dd7b5a4d4f53
OctopusHugz <colsonscott53@gmail.com>
2020-09-25 20:23:21 +0000

    Added files for exercise 2. Added checkbox and input to LIs. Made amenities h4 dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+const $ = window.$;
+const amenityDict = {};
+$(document).ready(function () {
+  $('li input').click(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').empty();
+    const amenityDictKeys = Object.keys(amenityDict);
+    const amenityDictLength = amenityDictKeys.length;
+    amenityDictKeys.forEach(function (key, index) {
+      $('.amenities h4').append(amenityDict[key]);
+      if (index !== amenityDictLength - 1) {
+        $('.amenities h4').append(', ');
+      }
+    });
+  });
+});
