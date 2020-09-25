8e976b1345d8335d75f0261ec103b7b31bdbabb0
Kelsie Merchant <eislek02@gmail.com>
2020-09-25 23:47:18 +0000

    Initial commit of files for task 5; files change routes/referenced files and new Javascript code will populate places based on filters when button is clicked

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,35 @@
+const $ = window.$;
+const amenityDict = {};
+$(document).ready(function () {
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+	if (data.status === 'OK') {
+	    $('#api_status').addClass('available');
+	} else {
+	    $('#api_status').removeClass('available');
+	}
+    });
+    $('li input').click(function () {
+	if (this.checked) {
+	    amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+	} else {
+	    delete amenityDict[$(this).attr('data-id')];
+	}
+	$('.amenities h4').empty();
+	const amenityDictKeys = Object.keys(amenityDict);
+	const amenityDictLength = amenityDictKeys.length;
+	amenityDictKeys.forEach(function (key, index) {
+	    $('.amenities h4').append(amenityDict[key]);
+	    if (index !== amenityDictLength - 1) {
+		$('.amenities h4').append(', ');
+	    }
+	});
+	if (amenityDictLength === 0) {
+	    $('.amenities h4').append('&nbsp;');
+	}
+    });
+
+    $('button').click(function () {
+	// HERE WILL BE A COPY OF WORK FROM PREVIOUS TASK
+	// BUT WITH amenityDict AS A FILTER
+    });
+});
