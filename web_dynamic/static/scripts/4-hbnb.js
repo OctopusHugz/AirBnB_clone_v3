c0f98f93884f79284744111b17c485ca6f7a8577
OctopusHugz <colsonscott53@gmail.com>
2020-09-26 00:49:57 +0000

    Fixed amenities typo and closing brace error

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -5,13 +5,13 @@ const infoString = '<div class="information"><div class="max_guest"></div><div c
 const descString = '<div class="description"></div></article>';
 const htmlString = titleString + infoString + descString;
 $(document).ready(function () {
-    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-	if (data.status === 'OK') {
-	    $('#api_status').addClass('available');
-	} else {
-	    $('#api_status').removeClass('available');
-	}
-    });
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
@@ -38,54 +38,54 @@ $(document).ready(function () {
       });
     }
   });
-    $('li input').click(function () {
-	if (this.checked) {
-	    amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
-	} else {
-	    delete amenityDict[$(this).attr('data-id')];
-	}
-	$('.amenities h4').empty();
-	const amenityDictKeys = Object.keys(amenityDict);
-	const amenityDictLength = amenityDictKeys.length;
-	amenityDictKeys.forEach(function (key, index) {
-	    $('.amenities h4').append(amenityDict[key]);
-	    if (index !== amenityDictLength - 1) {
-		$('.amenities h4').append(', ');
-	    }
-	});
-	if (amenityDictLength === 0) {
-	    $('.amenities h4').append('&nbsp;');
-	}
+  $('li input').click(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').empty();
+    const amenityDictKeys = Object.keys(amenityDict);
+    const amenityDictLength = amenityDictKeys.length;
+    amenityDictKeys.forEach(function (key, index) {
+      $('.amenities h4').append(amenityDict[key]);
+      if (index !== amenityDictLength - 1) {
+        $('.amenities h4').append(', ');
+      }
     });
-
-    $('button').click(function () {
-	const amenityDictKeys = Object.keys(amenityDict);
-	$.ajax({
-	    type: 'POST',
-	    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-	    data: JSON.stringify({ amenitites: amenityDictKeys }),
-	    dataType: 'json',
-	    contentType: 'application/json',
-	    success: function (data) {
-		$('section.places').empty();
-		data.forEach((place) => {
-		    $(htmlString).appendTo('section.places');
-		    $('.title_box h2').last().html(place.name);
-		    $('.title_box .price_by_night')
-			.last()
-			.html('$' + place.price_by_night);
-		    $('.information .max_guest')
-			.last()
-			.html(place.max_guest + ' Guests');
-		    $('.information .number_rooms')
-			.last()
-			.html(place.number_rooms + ' Rooms');
-		    $('.information .number_bathrooms')
-			.last()
-			.html(place.number_bathrooms + ' Bathrooms');
-		    $('.description').last().html(place.description);
-}
-		});
-	});
+    if (amenityDictLength === 0) {
+      $('.amenities h4').append('&nbsp;');
+    }
+  });
+  $('button').click(function () {
+    const amenityDictKeys = Object.keys(amenityDict);
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({ amenities: amenityDictKeys }),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (response) {
+        console.log(response);
+        $('section.places').empty();
+        response.forEach((place) => {
+          $(htmlString).appendTo('section.places');
+          $('.title_box h2').last().html(place.name);
+          $('.title_box .price_by_night')
+            .last()
+            .html('$' + place.price_by_night);
+          $('.information .max_guest')
+            .last()
+            .html(place.max_guest + ' Guests');
+          $('.information .number_rooms')
+            .last()
+            .html(place.number_rooms + ' Rooms');
+          $('.information .number_bathrooms')
+            .last()
+            .html(place.number_bathrooms + ' Bathrooms');
+          $('.description').last().html(place.description);
+        });
+      }
     });
+  });
 });
